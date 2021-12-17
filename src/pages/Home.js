80328d4bf069706c2cf3818094e4160dd20c78be
gabriel <gabrielcdonato@gmail.com>
2021-12-17 19:39:38 -0300

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react"; //useNomeDoHook
+import 
+    {View,
+     Text,
+     StyleSheet, 
+     TextInput,
+     Platform,
+     FlatList,
+     
+    } 
+     from "react-native";
+
+import { Button } from "../components/Button";
+import SkillCard from "../components/SkillCard";
+
+     export function Home(){
+
+         const [newSkill, setNewSkill] = useState('');
+         const [mySkills, setMySkills] = useState([]);
+         
+         //Usar o padrão handle quando for lidar com uma ação do usuario, no caso o usuario irá clicar e vai ocorrer a alteração de estado.
+
+         function handleAddNewSkill(){
+
+            setMySkills(oldState => [...oldState, newSkill]);
+            // ... 13mins usestate ignite cap 1, 2 conceitos importantes
+         }
+
+
+     return(
+         <View style = {styles.container} >
+
+             <Text style = {styles.title}>
+
+                Welcome, Rodrigo
+
+             </Text>
+
+             <TextInput style = {styles.input}
+                        placeholder = "New skill"
+                        placeholderTextColor = "#555" 
+                        onChangeText = {setNewSkill}      
+             />
+
+             <Button onPress = {handleAddNewSkill}/>
+
+             <Text style = {[ styles.title, { marginVertical: 50 }, ]} >
+                 My Skills
+             </Text>
+
+              <FlatList
+              showsVerticalScrollIndicator={false}
+                data={mySkills}
+                keyExtractor={item => item}
+                renderItem={({ item }) =>  (
+                    <SkillCard skill = {item} />
+                )}
+              />
+
+                
+             
+
+         </View>
+     )
+ }
+
+ const styles = StyleSheet.create({
+     container: {
+         flex: 1,
+         backgroundColor: '#121015',
+         paddingHorizontal: 20,
+         paddingVertical: 70,
+     },
+
+     title: {
+         color: '#fff',
+         fontSize: 24,
+         fontWeight: 'bold',
+     },
+
+     input: {
+         backgroundColor: '#1F1E25',
+         color: '#fff',
+         fontSize: 18,
+         padding: Platform.OS === 'ios' ? 15 : 10,
+         marginTop: 30,
+         borderRadius: 7
+     },
+
+ });
+
